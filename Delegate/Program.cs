3816e0edb93279ac483c41624e20766b4f4ae253
Aleksandr Trostyanko <ce-aleksandr.trostyanko@beyondnow.com>
2023-09-11 16:24:19 +0200

    initial

---
+++ Delegate/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Delegate;
+
+class Program
+{
+    // Тип делегата
+    public delegate void NoParameters();
+    
+    static void Main1()
+    {
+        // Экземпляр делегата
+        NoParameters noParameters1 = new NoParameters(ShowMessage);
+        NoParameters noParameters2 = new (ShowMessage);
+        NoParameters noParameters3 = ShowMessage;
+
+        noParameters1();
+        noParameters2();
+        noParameters3();
+    }
+
+    private static void ShowMessage()
+    {
+        Console.WriteLine("Have fun!!!");
+    }
+}
